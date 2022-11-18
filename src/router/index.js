2aefb39a013762e538972b14b388c414c7ff4dca
ontrigger <1350470775@qq.com>
2022-11-19 00:45:28 +0800

    同上一次

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import ElementUI from 'element-ui';
+import ElementUI, { Message } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import Login from '../components/Login.vue'
 import '../assets/css/global.css'
@@ -12,6 +12,7 @@ Vue.use(VueRouter);
 //配置请求的根路径
 axios.defaults.baseURL = 'http://1.15.141.230:8888/api/private/v1/'
 Vue.prototype.$http = axios;
+Vue.prototype.$message = Message;
 
 const routes = [
   { path: '/', redirect: '/login' },
